7c358d6cd238b4669a7655803b207a47ef2f6c2c
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-12-01 15:46:36 +0100

    first commit

---
+++ frontend/interview-prep-ai/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import LandingPage from "./pages/LandingPage";
+import InterviewPrep from "./pages/interviewPrep/components/InterviewPrep";
+import Dashboard from "./pages/home/Dashboard";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/interview-prep/:sessionId"
+            element={<InterviewPrep />}
+          />
+        </Routes>
+      </Router>
+
+      <Toaster toastOptions={{ className: "", style: { fontSize: "13px" } }} />
+    </div>
+  );
+};
+
+export default App;
