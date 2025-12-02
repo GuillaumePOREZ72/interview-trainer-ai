274264ccb111414d6174b9e7108d531e0dcf14d7
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-12-02 10:16:30 +0100

    ♻️ Refactor code structure for improved readability and maintainability

--- frontend/interview-prep-ai/src/pages/LandingPage.jsx
+++ frontend/interview-prep-ai/src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import HERO_IMG from "../assets/hero_img.png";
 import { APP_FEATURES } from "../utils/data";
 import { useNavigate } from "react-router-dom";
 import { LuSparkles } from "react-icons/lu";
@@ -64,6 +65,65 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="w-full min-h-full relative z-10 mb-56">
+        <div>
+          <section className="flex items-center justify-center -mt-36">
+            <img
+              src={HERO_IMG}
+              alt="Hero image"
+              className="w-[80vw] rounded-lg"
+            />
+          </section>
+        </div>
+      </div>
+
+      <div className="w-full min-h-full bg-[#fffcef] mt-10">
+        <div className="container mx-auto px-4 pt-10 pb-20">
+          <section className="mt-5">
+            <h2 className="text-2xl font-medium text-center mb-12">
+              {" "}
+              Features That Make You Shine
+            </h2>
+
+            <div className="flex flex-col items-center gap-8">
+              {/* First 3 cards */}
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
+                {APP_FEATURES.slice(0, 3).map((feature) => (
+                  <div
+                    key={feature.id}
+                    className="bg-[#fffef8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-100 transiotion border border-amber-100"
+                  >
+                    <h3 className="text-base font-semibold mb-3">
+                      {feature.title}
+                    </h3>
+                    <p className="text-gray-600">{feature.description}</p>
+                  </div>
+                ))}
+              </div>
+
+              {/* Remaining 2 cards */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {APP_FEATURES.slice(3).map((feature) => (
+                  <div
+                    key={feature.id}
+                    className="bg-[#fffef8] p-6 rounded-xl shadow-xs hover:shadow-lg shadow-amber-100 transition border border-amber-100"
+                  >
+                    <h3 className="text-base font-semibold mb-3">
+                      {feature.title}
+                    </h3>
+                    <p className="text-gray-600">{feature.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <div className="text-sm bg-gray-50 text-secondary text-center p-5 mt-5">
+        Made with ❤️... Happy Coding
       </div>
     </>
   );
